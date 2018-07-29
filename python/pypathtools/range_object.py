08284fe500f9316f23c11b710a85dfdd06c1f36b
Liam <liam.hoflay@gmail.com>
2018-07-29 18:53:48 +0100

    wip

--- python/pypathtools/range_object.py
+++ python/pypathtools/range_object.py
@@ -2,45 +2,63 @@ import itertools
 import re
 import types
 
-__all__ = ["RangeObject"]
+#__all__ = ["RangeObject"]
 
-class RangeNumber(object):
-	def __init__(self, value=0, sub_range=None):
-		self._value = value
-		self._sub_range = None
-	
-	@property
-	def sub_range(self):
-		return self._sub_range
+class SubRange(object):
+    def __init__(self, start=1, end=1, step=1):
+        self._start = start
+        self._end = end
+        self._step = step
+        self._sub_ranges = []
 
-	@sub_range.setter
-	def sub_range(self, sub_range):
-		pass
-	
+    @property
+    def sub_ranges(self):
+        return sorted(self._sub_ranges)
+    
+    def add_sub_range(self, sub_range):
+        self._sub_ranges.append(sub_range)
+
+    @classmethod
+    def from_list(cls, values):
+        pass
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        
+        pass
+        
+    def range(self):
+        for frame in xrange(self._start, self._end + 1, self._step):
+            for sub_frame in itertools.chain(*self.sub_ranges):
+                current = [frame]
+                current.extend(sub_frame)
+                yield current
+
 
 class RangeObject(object):
-	
+    
 
-	def __init__(self, values=None):
-		values = values or []
-		self._values = self._make_range(values)
+    def __init__(self, values=None):
+        values = values or []
+        self._values = self._make_range(values)
 
-	def __iter__(self):
-		pass
+    def __iter__(self):
+        pass
 
-	def __delitem__(self, index, stop):
-		pass
+    def __delitem__(self, index, stop):
+        pass
 
-	def __getitem__(self, index, stop):
-		pass
+    def __getitem__(self, index, stop):
+        pass
 
-	def __setitem__(self, index, stop):
-		pass
+    def __setitem__(self, index, stop):
+        pass
 
-	def __concat__(self, other):
-		new_range = []
-		pass
+    def __concat__(self, other):
+        new_range = []
+        pass
 
-	def __iconcat__(self, other):
-		self._range += other._range
-
+    def __iconcat__(self, other):
+        self._range += other._range
--- scratch/scratch.py
+++ scratch/scratch.py
@@ -13,32 +13,73 @@ regex = pad.get_regex_pattern()
 print regex
 match = re.search(regex, '/home/liam/foo.-1001.exr')
 if match:
-	print match.group(1)
+    print match.group(1)
 '''
 
 import itertools
 import random
+random.seed(1)
 
 data = []
 for i,j,k in itertools.combinations_with_replacement(xrange(1001,1010),3):
-	if random.choice([True, False]):
-		continue
-	data.append('{0}.{1}.{2}'.format(i,j,k))
+    if random.choice([True, False]):
+        continue
+    data.append('{0}.{1}.{2}'.format(i,j,k))
 
 
 results = {}
 
 
 for item in data:
-	components = item.split('.')
-	
-	current_data = results
-	for current in components:
-		if not current_data.get(current):
-			current_data[current] = {}
-		current_data = current_data[current]
-		
+    components = item.split('.')
+    
+    current_data = results
+    for current in components:
+        if not current_data.get(current):
+            current_data[current] = {}
+        current_data = current_data[current]
+        
 import pprint
-pprint.pprint(results)
+#pprint.pprint(results)
 
+'''
+def getRangeStr(items):
+    if not items:
+        return
+    def func(items):
+        return items[1] - items[0]
+    x = [int(a) for a in sorted(items)]
+
+    groups = []
+    prev = set()
+    for index, (id_, g) in enumerate(itertools.groupby(itertools.izip(x[:-1], x[1:]), func)):
+        this_grp = set()
+        for item in g:
+                this_grp.update(item)
+        if len(this_grp) <= 2 and id_ == 1:
+            continue
+        prev.difference_update(this_grp)
+        groups.append((id_, this_grp))
+        prev = this_grp
+
+    frame_str = ''
+    first = ''
+    for step, frange in groups:
+        frame_str += first
+        first = ','
+        if len(frange) == 1:
+                frame_str += '{0}'.format(next(iter(frange)))
+        elif step == 1:
+                frame_str += '{0}-{1}'.format(min(frange), max(frange))
+        else:
+                frame_str += '{0}-{1}x{2}'.format(min(frange), max(frange), step)
+    return frame_str
+#print getRangeStr(results.keys())
+'''
+from pypathtools.range_object import SubRange
+print 'foo'
+r1 = SubRange(1,10)
+r1.add_sub_range(SubRange(2, 8, 2))
+for item in r1.range():
+    print item
 
